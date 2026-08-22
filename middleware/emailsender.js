1463fdf25f02e6d7ae43b2576029dcf79a2e36b4
oluwajomiloju <oluwajomilojufaderin@gmail.com>
2026-08-22 20:08:24 +0100

    added family to emailSender

--- middleware/emailsender.js
+++ middleware/emailsender.js
@@ -1,11 +1,14 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    },
+    family: 4  // force IPv4
 });
 
 const sendEmail = async (to, subject, html) => {
